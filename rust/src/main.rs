01ead7d4bc94fe0bad514d5020dfe4b175bc3c33
Farzin <farzin.ahmadi@hotmail.com>
2026-02-11 02:44:40 -0400

    made delta encoder

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,11 +5,14 @@ use std::time::Instant;
 fn main() {
     let filename = "../data/books/the_complete_works_of_william_shakespeare_by_william_shakespeare.txt".to_string();
     let words = reader::read_content(filename);
+    let delta_encoding = reader::generate_freq(&words);
     let start_time = Instant::now();
     let duration = start_time.elapsed();
     println!("Time taken to read file: {:?}", duration);
     println!("Length of words: {}", words.len());
     println!("First 10 words: {:?}", words.iter().take(10).collect::<Vec<&String>>());
+    //i asked gpt to make 
+    delta_encoding.iter().take(2).for_each(|(w, v)| println!("{w} -> {:?}", v));
     let end_time = Instant::now();
     let duration = end_time.elapsed();
     println!("Time taken to print words: {:?}", duration);
--- rust/src/reader.rs
+++ rust/src/reader.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 pub fn read_content(filename: String) -> Vec<String> {
     let contents = fs::read_to_string(filename).unwrap();
@@ -14,4 +15,24 @@ pub fn read_content(filename: String) -> Vec<String> {
         .collect();
 
     words
+}
+
+//we make this function a life time. Meaning, we make a promise that the values of str wont be called after the word array
+//dissapears. Instead of having a hashmap with more strings, we use their reference (pointers)
+pub fn generate_freq<'a>(word_array: &'a Vec<String>) -> HashMap<&'a str, Vec<i32>>{
+    let mut delta_encoding: HashMap<&'a str, Vec<i32>> = HashMap::new();
+    //Enumerate is created from an iter. The reason why this is necessary because you need to explicitly define what
+    //you want to do. For example, a regular iter is a read only reference, iter_mut is a mutable reference
+    for(i, string) in word_array.iter().enumerate() {
+        delta_encoding
+            .entry(string)
+            .and_modify(|vec|{
+                let last_pos = *vec.last().unwrap();
+                vec.push(i as i32 - last_pos);
+            }) 
+            //makes the default value
+            .or_default()
+            .push(i as i32);
+    } 
+    delta_encoding
 }
