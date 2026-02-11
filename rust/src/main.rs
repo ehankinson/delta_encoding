5c88c47c46fbfcb54bbe85077d48b710850ed49b
Ethan Hankinson <esh64887@gmail.com>
2026-02-11 14:28:11 -0400

    Fixed issue with merge

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,9 +7,8 @@ use crate::constants::Codec;
 
 fn main() {
     let filename = "../data/books/the_complete_works_of_william_shakespeare_by_william_shakespeare.txt".to_string();
-    let words = reader::read_content(filename);
-    let delta_encoding = greader::generate_freq(words);
+    let words = reader::read_content(filename).expect("failed to read input file");
     let codec = Codec::BytePack;
-    let _ = writer::write_postings(codec, delta_encoding);
+    let _ = writer::write_postings(codec, words);
 
 }
