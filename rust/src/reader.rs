c0a747ced764048931afedd634ca3ed493b4b764
Farzin <farzin.ahmadi@hotmail.com>
2026-02-11 14:05:00 -0400

    file clean up

--- rust/src/reader.rs
+++ rust/src/reader.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 
 pub fn read_content(filename: String) -> std::io::Result<HashMap<String, Vec<i32>>> {
@@ -13,7 +13,7 @@ pub fn read_content(filename: String) -> std::io::Result<HashMap<String, Vec<i32
         let line = lines?;
 
         for word in line.split_whitespace(){
-            
+
             let clean_word: String = word.chars()
             .filter(|c| c.is_alphanumeric())
             .flat_map(|c| c.to_lowercase())
@@ -42,25 +42,3 @@ pub fn encode_deltas(map: &mut HashMap<String, Vec<i32>>){
         }
     }
 }
-
-// maybe 
-
-//we make this function a life time. Meaning, we make a promise that the values of str wont be called after the word array
-//dissapears. Instead of having a hashmap with more strings, we use their reference (pointers)
-// pub fn generate_freq(word_array: Vec<String>) -> HashMap< String, Vec<i32>>{
-//     let mut delta_encoding: HashMap<String, Vec<i32>> = HashMap::new();
-//     //Enumerate is created from an iter. The reason why this is necessary because you need to explicitly define what
-//     //you want to do. For example, a regular iter is a read only reference, iter_mut is a mutable reference
-//     for(i, string) in word_array.into_iter().enumerate() {
-//         delta_encoding
-//             .entry(string)
-//             .and_modify(|vec|{
-//                 let last_pos = *vec.last().unwrap();
-//                 vec.push(i as i32 - last_pos);
-//             }) 
-//             //makes the default value
-//             .or_default()
-//             .push(i as i32);
-//     } 
-//     delta_encoding
-// }
