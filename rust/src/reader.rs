6f65a514cda06d94fed497c72ce199bc0fbdf628
Farzin <farzin.ahmadi@hotmail.com>
2026-02-11 11:59:46 -0400

    minor ownership change

--- rust/src/reader.rs
+++ rust/src/reader.rs
@@ -19,11 +19,11 @@ pub fn read_content(filename: String) -> Vec<String> {
 
 //we make this function a life time. Meaning, we make a promise that the values of str wont be called after the word array
 //dissapears. Instead of having a hashmap with more strings, we use their reference (pointers)
-pub fn generate_freq<'a>(word_array: &'a Vec<String>) -> HashMap<&'a str, Vec<i32>>{
-    let mut delta_encoding: HashMap<&'a str, Vec<i32>> = HashMap::new();
+pub fn generate_freq(word_array: Vec<String>) -> HashMap< String, Vec<i32>>{
+    let mut delta_encoding: HashMap<String, Vec<i32>> = HashMap::new();
     //Enumerate is created from an iter. The reason why this is necessary because you need to explicitly define what
     //you want to do. For example, a regular iter is a read only reference, iter_mut is a mutable reference
-    for(i, string) in word_array.iter().enumerate() {
+    for(i, string) in word_array.into_iter().enumerate() {
         delta_encoding
             .entry(string)
             .and_modify(|vec|{
